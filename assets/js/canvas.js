ce71afee70c7891c068c934884b0d2c37fb1260a
Amiel Monasterial <amielmon@gmail.com>
2018-07-08 18:01:45 -0400

    Update canvas.js

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -12,7 +12,7 @@ $(function() {
   canvas.style.display = 'block';
 
   context.fillStyle = color;
-  context.lineWidth = 0.2;
+  context.lineWidth = 1;
   context.strokeStyle = color;
 
   for (var i = 0; i < count; i++) {
